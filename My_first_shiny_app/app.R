a5fa12ebcbd441624ea12b926c15895eb68ff989
Neil Rankin <Neil Rankin>
2017-02-17 11:45:46 +0200

    facet plot

--- My_first_shiny_app/app.R
+++ My_first_shiny_app/app.R
@@ -8,40 +8,46 @@
 #
 
 library(shiny)
+library(dplyr)
+library(ggplot2)
+library(southafricastats)
+
+mortality <-mortality_zaf %>% filter((indicator != "All causes"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("South Africa stats"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput(inputId = "province",
+                     label = "Choose province:",
+                     choices = unique(mortality_zaf$province), 
+                     selected = "Gauteng", 
+                     multiple = TRUE)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("LinePlot")
       )
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required 
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'grey', border = 'black')
+   output$LinePlot <- renderPlot({
+     mortality %>% filter(province %in% input$province) %>% 
+       ggplot(aes(year, deaths, colour = indicator)) + 
+       facet_wrap(~province) + 
+       geom_line(alpha = 0.8, size=1) 
+     
+     
+     
    })
 }
 
